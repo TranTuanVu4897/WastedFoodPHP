0f2896a345574a4911f38eed5a5e40bb3ea3130c
TranTuanVu4897 <46296441+TranTuanVu4897@users.noreply.github.com>
2020-10-16 20:52:21 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "localhost";
+$server = "localhost";
 $username = "root";
 $password = "";
 $dbname = "wasted_food_database";
 
-$connect = mysqli_connect($servername, $username, $password, $dbname);
+$connect = mysqli_connect($server, $username, $password, $dbname);
 mysqli_query($connect, "SET NAMES 'utf8'");
 
 //Check connection
