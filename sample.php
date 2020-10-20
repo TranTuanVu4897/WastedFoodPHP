3f28620efc96c93c85810522561b25cbe4d7b4a4
TranTuanVu4897 <46296441+TranTuanVu4897@users.noreply.github.com>
2020-10-21 00:16:45 +0700

    create login 3rd party

--- sample.php
+++ sample.php
@@ -1,9 +1,2 @@
 <?php
-$firstNumber = 1;
-$secondNumber = 2;
-$sum = $firstNumber + $secondNumber;
-if (2 === $sum) {
-    echo "it's wrong baby";
-} else {
-    echo "You right, baby";
-}
+ phpinfo();
