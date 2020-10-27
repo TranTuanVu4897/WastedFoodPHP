89c64b60cca03ecfa209efe0b249082c647bf5ed
tungtim2401 <62582860+tungtim2401@users.noreply.github.com>
2020-10-27 15:33:29 +0700

    Update register3rdParty.php

--- login/register3rdParty.php
+++ login/register3rdParty.php
@@ -4,31 +4,26 @@ require "../connection.php";
 //get 3rd party id
 
 
-$thirdPartyId = $_REQUEST["thirdPartyId"];
+$thirdPartyId = $_POST["thirdPartyId"];
+$name = $_POST["name"];
+$urlImage = $_POST["urlImage"];
+$dob = $_POST["dob"];
+$gender = $_POST["gender"];
+$email = $_POST["email"];
 
-$account_id  = $_REQUEST["account_id"];
-$name = $_REQUEST["name"];
-$phone = $_REQUEST["phone"];
-$image = $_REQUEST["image"];
-$dob = $_REQUEST["dob"];
-$gender = $_REQUEST["gender"];
-$email = $_REQUEST["email"];
-
+// $thirdPartyId = "tung11918";
+// $name ="Nguyen Ngoc Anh";
+// $urlImage = "";
+// $dob = "11/11/1111";
+// $gender = "Nữ";
+// $email = "anh@gmail.com";
 
 $thirdPartyId = mysqli_real_escape_string($connect,$thirdPartyId);
-$account_id = mysqli_real_escape_string($connect, $id);
 $name = mysqli_real_escape_string($connect, $name);
-$image = mysqli_real_escape_string($connect, $image);
-$dob = mysqli_real_escape_string($connect, $image);
+$urlImage = mysqli_real_escape_string($connect, $urlImage);
+$dob = mysqli_real_escape_string($connect, $dob);
 $gender = mysqli_real_escape_string($connect, $gender);
 $email = mysqli_real_escape_string($connect, $email);
-
-$thirdPartyId = $_REQUEST['thirdPartyId'];
-$image = $_REQUEST['account_id'];
-$dob = $_REQUEST['dob'];
-$gender = $_REQUEST['gender'];
-$name = $_REQUEST['name'];
-$email = $_REQUEST['email'];
 
 
 $query1 = "SELECT `id` FROM `account` WHERE `third_party_id` = '$thirdPartyId'";
@@ -37,7 +32,7 @@ $result = $connect->query($query1);
 
 
 //3rd party id exist -> login
-if($result->num_rows<=0){
+if($result->num_rows>0){
     //return OK
     echo "OK";
     exit();
@@ -47,18 +42,27 @@ else{
     //3rd party id not exist -> register
     $query2 = "SELECT COUNT(`id`) FROM `account`";
     $result = $connect->query($query2);
-    $count = $row[0];
-    //take count for get number
-    $username = "test" . $count;
-    $id = "30" . $count;
+    while($row = mysqli_fetch_row($result)){
+        $count = $row[0];
+    }
 
+    
+    
+    //take count for get number
+    // $username = "test" . $count;
+    $id = "30" . $count;
     //insert into account
     $query3 = "INSERT INTO `account` (`id`, `role_id`, `username`, `password`, `phone`, `third_party_id`, `email`, `created_date`, `is_active`, `modified_date`)
-    VALUES ('$id', '3', '$username', '$username', '011223345', $thirdPartyId, $email, current_timestamp(), '1', current_timestamp());";
-    $result = $connect->query($query3);
+    VALUES ('$id', '3', '$username', '$username', NULL, '$thirdPartyId', '$email', current_timestamp(), '1', current_timestamp())";
+    //$result = $connect->query($query3);
+    if(mysqli_query($connect, $query3)){
+        echo "success";
+    } else{
+        echo"error";
+    }
     //insert into buyer
     $query4 = "INSERT INTO `buyer` (`account_id`, `date_of_birth`, `image`, `gender`, `modified_date`, `name`)
-    VALUES ('$id', '$dob', ''$image'', NULL, current_timestamp(), '$name')";
+    VALUES ('$id', '$dob', '$urlImage', '$gender', current_timestamp(), '$name')";
     $result = $connect->query($query4);
 }
 ?>
