d239d5779cd056d1fb027ce87dc3b547d100dd1e
tungtim2401 <62582860+tungtim2401@users.noreply.github.com>
2020-11-06 22:58:42 +0700

    update

--- connection.php
+++ connection.php
@@ -1,17 +1,17 @@
 <?php
 //This is for server on heroku
-$server = "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$username = "opz3hmxpxk0vfl21";
-$password = "swc44g1p3ciko317";
-$dbname = "eaur5hgdarhpvk2e";
-$port = "3306";
-
-
-// $server = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "wasted_food_database";
+// $server = "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+// $username = "opz3hmxpxk0vfl21";
+// $password = "swc44g1p3ciko317";
+// $dbname = "eaur5hgdarhpvk2e";
 // $port = "3306";
+
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "wasted_food_database";
+$port = "3306";
 
 $connect = mysqli_connect($server, $username, $password, $dbname, $port);
 
