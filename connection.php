ee5f55fe155714a9a3a074b31c52dc015f41529e
TranTuanVu4897 <46296441+TranTuanVu4897@users.noreply.github.com>
2020-11-06 21:18:46 +0700

    test deploy

--- connection.php
+++ connection.php
@@ -1,13 +1,17 @@
 <?php
-$server = "localhost";
-$username = "root";
-$password = "";
-$dbname = "wasted_food_database";
+$server = "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "opz3hmxpxk0vfl21";
+$password = "swc44g1p3ciko317";
+$dbname = "eaur5hgdarhpvk2e";
+$port = "3306";
 
-$connect = mysqli_connect($server, $username, $password, $dbname);
+$connect = mysqli_connect($server, $username, $password, $dbname, $port);
+
 mysqli_query($connect, "SET NAMES 'utf8'");
 
 //Check connection
 if ($connect->connect_error) {
     die("Connection failed: " . $connect->connect_error);
+    echo "Connection failed";
 }
+echo "success";
