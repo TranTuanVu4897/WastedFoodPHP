ce38456946434343cee773d986fed39bd3f8f1f6
TranTuanVu4897 <46296441+TranTuanVu4897@users.noreply.github.com>
2020-11-18 19:40:17 +0700

    fix seller order detail history

--- seller/getListOrderSeller.php
+++ seller/getListOrderSeller.php
@@ -15,27 +15,50 @@ INNER JOIN seller
 on product.seller_id = seller.account_id
 INNER JOIN buyer
 on order.buyer_id = buyer.account_id
-where seller.account_id = $seller_id and product_id = $product_id  and order.status = $order_status" ;
+where seller.account_id = $seller_id and product_id = $product_id  and order.status = $order_status";
+
+$query = "  SELECT `order`.id,
+                `order`.`buyer_id`,
+                `buyer`.`name` AS `buyer_name`,
+                `product`.`name`,
+                `order`.`product_id`,
+                `order`.`quantity`,
+                `order`.`status`,
+                `order`.`total_cost`,
+                `order`.`buyer_rating`,
+                `order`.`buyer_comment`,
+                `order`.`modified_date`,
+                `buyer`.`image`
+            FROM `order`
+            INNER JOIN `product` 
+                    ON `product_id` = `product`.`id`
+            INNER JOIN `seller`
+                    ON `product`.`seller_id` = `seller`.`account_id`
+            INNER JOIN `buyer` 
+                    ON `order`.`buyer_id` = `buyer`.`account_id`
+            WHERE
+                    `seller`.`account_id` = $seller_id 
+                    AND `product_id` = $product_id 
+                    AND `order`.`status` = '$order_status'";
 
 $result = $connect->query($query);
 
 //Class order
 class Order
 {
-    function Order($id, $buyer_id, $buyerName, $nameProduct, $product_id, $quantity, $status, $total_cost, $buyer_rating, $buyer_comment, $modified_date , $image)
+    function Order($id, $buyer_id, $buyerName, $nameProduct, $product_id, $quantity, $status, $total_cost, $buyer_rating, $buyer_comment, $image)
     {
         $this->id = $id;
         $this->buyer_id = $buyer_id;
-        $this->buyerName = $buyerName;
-        $this->nameProduct = $nameProduct;
+        $this->buyer_name = $buyerName;
+        $this->name_product = $nameProduct;
         $this->product_id = $product_id;
         $this->quantity = $quantity;
         $this->status = $status;
         $this->total_cost = $total_cost;
         $this->buyer_rating = $buyer_rating;
         $this->buyer_comment = $buyer_comment;
-        $this->modified_date = $modified_date;
-        $this->image = $image;
+        $this->buyer_avatar = $image;
     }
 }
 
@@ -43,13 +66,8 @@ class Order
 $listOrder = array();
 
 while ($row = mysqli_fetch_assoc($result)) {
-    array_push($listOrder, new Order($row['id'], $row['buyer_id'], $row['buyer_name']  , $row['name'] ,  $row['product_id'], $row['quantity'], $row['status'], $row['total_cost'], $row['buyer_rating'], $row['buyer_comment'], $row['modified_date'], $row['image']));
+    array_push($listOrder, new Order($row['id'], $row['buyer_id'], $row['buyer_name'], $row['name'],  $row['product_id'], $row['quantity'], $row['status'], $row['total_cost'], $row['buyer_rating'], $row['buyer_comment'], $row['image']));
 }
 
 //change array to json
 echo json_encode($listOrder);
-
-
-
-
-
