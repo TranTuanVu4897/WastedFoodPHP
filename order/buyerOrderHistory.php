baae72b79d03ad4b30744f869ed0f5e4b113104d
TranTuanVu4897 <46296441+TranTuanVu4897@users.noreply.github.com>
2020-12-02 13:27:16 +0700

    Update buyerOrderHistory.php

--- order/buyerOrderHistory.php
+++ order/buyerOrderHistory.php
@@ -17,7 +17,7 @@ $query = "SELECT `order`.`id` AS `order_id`,`buyer_id`,`product_id`,`quantity`,
         `start_time`, `end_time`,`original_price`, `sell_price`, `original_quantity` , 
         `remain_quantity` , `product`.`description` AS `product_description`, `sell_date` , `product`.`status` AS `product_status`,`shippable`,
         `account_id`,`seller`.`name` AS `seller_name`,`seller`.`image` AS `seller_image`,
-        `address`,`latitude`,`longitude`,`seller`.`description` AS `seller_description`
+        `address`,`latitude`,`longitude`,`seller`.`description` AS `seller_description`, `rating`
 FROM `order` JOIN `product` ON `product`.`id` = `order`.`product_id` 
 JOIN `seller` ON `seller`.`account_id` = `product`.`seller_id` 
 WHERE `buyer_id` = $buyer_id";
@@ -27,7 +27,7 @@ $result = $connect->query($query);
 $listOrderProducts = array();
 
 while ($row = mysqli_fetch_assoc($result)) {
-    $seller = new Seller($row['account_id'], null, null, null, null, null, null, null, null, $row['seller_name'], $row['seller_image'], $row['address'], $row['latitude'], $row['longitude'], $row['seller_description'], null);
+    $seller = new Seller($row['account_id'], null, null, null, null, null, null, null,null, null, $row['seller_name'], $row['seller_image'], $row['address'], $row['latitude'], $row['longitude'], $row['seller_description'], null,$row['rating']);
     $product = new Product($row['product_id'], $row['seller_id'], $row['product_name'], $row['product_image'], $row['start_time'], $row['end_time'], $row['original_price'], $row['sell_price'], $row['original_quantity'], $row['remain_quantity'], $row['product_description'], $row['sell_date'], $row['product_status'], $row['shippable'], $seller);
     array_push($listOrderProducts, new Order($row['order_id'], $row['buyer_id'], $row['product_id'], $row['quantity'], $row['order_status'], $row['total_cost'], $row['buyer_comment'],$product));
 }
