d81a7b983a15537e4f4c416da9947c903a48a256
TranTuanVu4897 <46296441+TranTuanVu4897@users.noreply.github.com>
2020-12-04 04:37:17 +0700

    fix

--- information/getBuyerBy3rdPartyId.php
+++ information/getBuyerBy3rdPartyId.php
@@ -3,7 +3,7 @@ require "../connection.php";
 require "../model/buyer.php";
 
 //get username and password from url parameters
-$third_party_id = $_REQUEST['thirdPartyID'];
+$third_party_id = $_REQUEST['thirdPartyId'];
 
 
 $third_party_id = mysqli_real_escape_string($connect, $third_party_id);
@@ -19,7 +19,7 @@ $query = <<<EOF
         ON `account`.`id` = `buyer`.`account_id` 
         WHERE `third_party_id` = '$third_party_id' ;
 EOF;
-echo $query;
+
 // $result = $connect->query($query);
 $result = mysqli_query($connect,$query) or trigger_error("Query Failed! SQL: $query - Error: ".mysqli_error($connect), E_USER_ERROR);
 
