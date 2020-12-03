0fb3655983078956a2f819cf755644e607679f40
TranTuanVu4897 <46296441+TranTuanVu4897@users.noreply.github.com>
2020-12-04 02:02:31 +0700

    Update getListSellerFollow.php

--- information/getListSellerFollow.php
+++ information/getListSellerFollow.php
@@ -12,7 +12,7 @@ $buyer_id = $_REQUEST['buyer_id'];
 //remove special string from parameters
 $buyer_id = mysqli_real_escape_string($connect, $buyer_id);
 
-$query = "SELECT `account_id`,`name`,`image`,`address`,`latitude`,`longitude`,`description` FROM `seller` INNER JOIN `list_follow` ON seller.account_id = list_follow.seller_id WHERE list_follow.buyer_id = $buyer_id";
+$query = "SELECT `account_id`,`name`,`image`,`address`,`latitude`,`longitude`,`description`,`rating` FROM `seller` INNER JOIN `list_follow` ON `seller`.`account_id` = `list_follow`.`seller_id` WHERE `list_follow`.`buyer_id` = $buyer_id AND `list_follow`.`is_follow` = 1";
 $result = $connect->query($query);
 
 
@@ -20,9 +20,8 @@ $result = $connect->query($query);
 $listSeller = array();
 
 while($row = mysqli_fetch_assoc($result)){
-    array_push($listSeller, new Seller($row['account_id'],null,null,null,null,null,null,null,null, $row['name'], $row['image'], $row['address'], $row['latitude'], $row['longitude'], $row['description']));
+    array_push($listSeller, new Seller($row['account_id'],null,null,null,null,null,null,null,null,null, $row['name'], $row['image'], $row['address'], $row['latitude'], $row['longitude'], $row['description'],null,$row['rating']));
 }
 //return json object if not error
 echo json_encode($listSeller) . "<br/>";
 $connect->close();
-?>
