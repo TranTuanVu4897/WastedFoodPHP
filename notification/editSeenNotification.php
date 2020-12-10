311931e323cb256a490c978a525ff919f4113134
trnhnhatminh <57166086+trnhnhatminh@users.noreply.github.com>
2020-12-10 13:33:22 +0700

    Update editSeenNotification.php

--- notification/editSeenNotification.php
+++ notification/editSeenNotification.php
@@ -3,9 +3,9 @@ require "../connection.php";
 $receiver_id = "2001";
 // $receiver_id = $_POST["receiver_id"];
 
-$query = "update Notification set 
-seen = TRUE
-where receiver_id = '$receiver_id'";
+$query = "update `notification` set 
+`notification`.`seen` = TRUE
+where `notification`.`receiver_id` = '$receiver_id'";
 echo $query;
 //$result = mysqli_query($connect,$query) or trigger_error("Query Failed! SQL: $query - Error: ".mysqli_error($connect), E_USER_ERROR);
 if(mysqli_query($connect,$query))
