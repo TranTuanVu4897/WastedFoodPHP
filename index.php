7103402fb13cd8310f14b80e509306a1db690411
TranTuanVu4897 <46296441+TranTuanVu4897@users.noreply.github.com>
2020-12-18 17:14:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ echo "Welcome user 8082. Wish you live forever.";
 require 'connection.php';
 $result = mysqli_query($connect, "Select now();");
 while ($row = mysqli_fetch_assoc($result)) {
-    
+    echo $row[0];
 }
