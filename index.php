b6ca4d055662e2d8f7ad826c4eafc561664ed9c4
TranTuanVu4897 <46296441+TranTuanVu4897@users.noreply.github.com>
2020-12-18 17:18:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ echo "Welcome user 8082. Wish you live forever.";
 require 'connection.php';
 $query = "Select now();";
 $result = mysqli_query($connect,$query) or trigger_error("Query Failed! SQL: $query - Error: ".mysqli_error($connect), E_USER_ERROR);
-$result->num_rows;
+echo $result->num_rows;
 while ($row = mysqli_fetch_assoc($result)) {
     echo $row[0];
 }
